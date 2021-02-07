1622cfa61928f00e83db2bd53b3af06cb3bb1e0a
Henrique Paulino Cruz <49167008+HenriqueCruz5341@users.noreply.github.com>
2021-02-07 10:08:29 -0300

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -37,7 +37,9 @@ sudo apt -y install insomnia
 #========================================================================================
 
 # ===| Spotify |===
-snap install spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install -y spotify-client
 #========================================================================================
 
 # ===| MongoDB Compass |===
