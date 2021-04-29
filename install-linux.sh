f17f818de4c15f7ee8b4282d98e8a66e85a4c5b1
Henrique Paulino Cruz <49167008+HenriqueCruz5341@users.noreply.github.com>
2021-04-29 11:15:46 -0300

    Update install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -36,6 +36,10 @@ sudo apt update
 sudo apt -y install insomnia 
 #========================================================================================
 
+# ===| Postman |===
+sudo snap install postman
+#========================================================================================
+
 # ===| Spotify |===
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
